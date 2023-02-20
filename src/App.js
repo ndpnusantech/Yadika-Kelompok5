5c35950120c9462fce672a2a709036ca04a51ce1
Nurhasna Fadhilah <nurhasnafadhilah00@gmail.com>
2023-02-20 15:17:47 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Mainpage from './components/Page/MainPage/MainPage';
-import ProfilMobil from './components/Page/ProfilMobil/ProfilMobil';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Mainpage />} />
-        <Route path="/profilMobil" element={<ProfilMobil />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
